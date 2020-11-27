9b64ce514142cce825020a50ccc422ef0a801598
kobold <kovacsboldizsar@gmail.com>
2020-11-27 14:35:06 +0200

    Basic parenthesis. Corner cases need attention

--- src/main/java/ComputeExpression.java
+++ src/main/java/ComputeExpression.java
@@ -1,53 +1,121 @@
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.LinkedList;
 
 public class ComputeExpression {
-
-    private static final String e = "4+2*3*6/9*4-4*(4+1)";
+    private static final int PP = 5;
+    private static final String e = "4+2*3*6/9*4-4*(4+2*2)";
 
     public static void main(String[] args) {
 
-        LinkedList<Node> openTrees = new LinkedList<>();
+        Node root = parseIntoTree();
+
+        int result = compute(root);
+    }
+
+    private static int compute(Node root) {
+        if (root instanceof OperandNode)
+            return (int) root.value;
+
+        OperatorNode operatorNode = (OperatorNode) root;
+        if (operatorNode.value == Op.ADD)
+            return compute(operatorNode.left) + compute(operatorNode.right);
+        if (operatorNode.value == Op.SUB)
+            return compute(operatorNode.left) - compute(operatorNode.right);
+        if (operatorNode.value == Op.MUL)
+            return compute(operatorNode.left) * compute(operatorNode.right);
+        if (operatorNode.value == Op.DIV)
+            return compute(operatorNode.left) / compute(operatorNode.right);
+        return 0;
+    }
+
+    private static Node parseIntoTree() {
+        LinkedList<OperatorNode> openTrees = new LinkedList<>();
+        int parenthesisDepth = 0;
 
         for (int i = 0; i < e.length(); i++) {
-            Integer operand = Character.getNumericValue(e.charAt(i));
-            Node operandNode = new Node(operand, null, null);
-            i++;
-            char operatorSymbol = e.charAt(i);
-            Node operatorNode = new Node(Op.fromChar(operatorSymbol), null, null);
+            char ch = e.charAt(i);
+            if (ch == '(') {
+                parenthesisDepth++;
+                continue;
+            } else if (ch == ')') {
+                parenthesisDepth--;
+                continue;
+            }
+
+            Integer operand = Character.getNumericValue(ch);
+            OperandNode operandNode = new OperandNode(operand);
+            char operatorSymbol = e.charAt(++i);
+            OperatorNode operatorNode = new OperatorNode(Op.fromChar(operatorSymbol), parenthesisDepth);
 
             if (openTrees.isEmpty()) { // starting out
                 operatorNode.left = operandNode;
                 openTrees.addLast(operatorNode);
                 continue;
             }
-            Node lastOpenTree = openTrees.peekLast();
-            int prioLastOpen, prioCurrent;
-            if ((prioLastOpen = ((Op) lastOpenTree.value).prio) <
-                    (prioCurrent = ((Op)operatorNode.value).prio)) { // current op is stronger than last open tree
+            OperatorNode lastOpenTree = openTrees.peekLast();
+            if (operatorSymbol == ')') {
+                lastOpenTree.right = operandNode;
+                Node<?> currentlyLinkedSubnode = operandNode;
+                while (openTrees.peekLast().getParenthesisDepth() == parenthesisDepth) {
+                    openTrees.peekLast().right = currentlyLinkedSubnode;
+                    currentlyLinkedSubnode = openTrees.removeLast();
+                }
+                openTrees.peekLast().right = currentlyLinkedSubnode;
+                parenthesisDepth--;
+                continue;
+            }
+            int prioLastOpen = lastOpenTree.getPrio();
+            int prioCurrent = operatorNode.getPrio();
+            if (prioLastOpen < prioCurrent) { // current op is stronger than last open tree
                 operatorNode.left = operandNode;
                 openTrees.addLast(operatorNode);
             } else if (prioLastOpen == prioCurrent) { // current op is same strength as last open tree
-                Node lastOpen = openTrees.removeLast();
+                OperatorNode lastOpen = openTrees.removeLast();
                 lastOpen.right = operandNode;
                 operatorNode.left = lastOpen;
                 openTrees.addLast(operatorNode);
             } else { // current op is weaker than last open tree
-                Node lastOpen = openTrees.removeLast();
+                OperatorNode lastOpen = openTrees.removeLast();
                 lastOpen.right = operandNode;
-                Node prevLastOpen = openTrees.removeLast();
+                OperatorNode prevLastOpen = openTrees.removeLast();
                 prevLastOpen.right = lastOpen;
                 operatorNode.left = prevLastOpen;
                 openTrees.addLast(operatorNode);
             }
         }
+
+        openTrees.get(0).right = openTrees.removeLast();
+        return openTrees.get(0);
     }
 
-    @AllArgsConstructor
-    private static class Node {
-        private Object value;
-        private Node left, right;
+    private static class Node<T> {
+        protected final T value;
+        protected Node<?> left, right;
+
+        Node(T value) {
+            this.value = value;
+        }
+    }
+
+    private static class OperandNode extends Node<Integer> {
+        public OperandNode(Integer value) {
+            super(value);
+        }
+    }
+
+    private static class OperatorNode extends Node<Op> {
+        @Getter
+        private final int parenthesisDepth;
+        private OperatorNode(Op value, int parenthesisDepth) {
+            super(value);
+            this.parenthesisDepth = parenthesisDepth;
+        }
+
+        private int getPrio() {
+            return parenthesisDepth * PP + value.prio;
+        }
     }
 
     private enum Op {
@@ -57,6 +125,7 @@ public class ComputeExpression {
         DIV(1);
 
         private final int prio;
+
         Op(int prio) {
             this.prio = prio;
         }
